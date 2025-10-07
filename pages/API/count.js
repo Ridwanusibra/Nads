1d1608d1f114cbfb545c836e046119a02b25b424
Ridwanusibra <167793564+Ridwanusibra@users.noreply.github.com>
2025-10-07 11:34:12 +0100

    Create count.js

---
+++ pages/API/count.js
@@ -0,0 +1,5 @@
+export default async function handler(req, res) {
+  // Fake data (replace later with Twitter API if you want)
+  const randomCount = Math.floor(Math.random() * 1000);
+  res.status(200).json({ count: randomCount });
+}￼Enter
